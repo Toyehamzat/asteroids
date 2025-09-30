457fd94c0deacfa9824890be3ff7e87aad420811
Toyehamzat <anibiseun@gmail.com>
2025-09-30 15:34:41 +0100

    Refactor player shooting mechanism to improve timer management

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -27,6 +29,10 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        
+        # Decrease the timer by dt every frame
+        if self.timer > 0:
+            self.timer -= dt
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             self.rotate(-dt)  # Negative dt to rotate left
@@ -34,6 +40,25 @@ class Player(CircleShape):
             self.rotate(dt)   # Positive dt to rotate right
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.move(dt)
-        # if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        #     self.move(-dt)  # Move backward
+        if keys[pygame.K_SPACE] or keys[pygame.K_KP_ENTER]:
+             self.shoot()
+
+    def shoot(self):
+        # Prevent shooting if timer is greater than 0
+        if self.timer > 0:
+            return
+            
+        # Create a new shot at the position of the player
+        shot = Shot(self.position.x, self.position.y)
+        
+        # Set the shot's velocity:
+        # Start with a pygame.Vector2 of (0, 1)
+        velocity = pygame.Vector2(0, 1)
+        # Rotate it in the direction the player is facing
+        velocity = velocity.rotate(self.rotation)
+        # Scale it up (multiply by PLAYER_SHOOT_SPEED) to make it move faster
+        shot.velocity = velocity * PLAYER_SHOOT_SPEED
+        
+        # Set the cooldown timer
+        self.timer = PLAYER_SHOOT_COOLDOWN
     
